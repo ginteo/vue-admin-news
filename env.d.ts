468cc0dab0d480048010ed0ce4c46e8d624a234d
gin <215129113@qq.com>
2022-10-02 17:14:19 +0800

    feat(axios/config): configure axios baseURL

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_DEV_BASE_URL: string
   readonly VITE_PROD_BASE_URL: string
+  readonly VITE_DEV_PREFIX_URL: string
+  readonly VITE_PROD_PREFIX_URL: string
 }
 
 interface ImportMeta {
--- src/plugins/axios/config.ts
+++ src/plugins/axios/config.ts
@@ -1,6 +1,9 @@
 let baseURL = ''
 const timeout = 1000
 
-baseURL = import.meta.env.MODE === 'development' ? '/api/v1/admin' : '/'
+baseURL =
+  import.meta.env.MODE === 'development'
+    ? import.meta.env.VITE_DEV_PREFIX_URL
+    : import.meta.env.VITE_PROD_PREFIX_URL
 
 export { baseURL, timeout }
